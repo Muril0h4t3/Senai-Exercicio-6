09ea440cff877de9de5f35076b1919a57194d667
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-09-27 10:00:07 -0300

    msg

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace senai.operadores.ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Informe o valor em real");
+            decimal vr = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor em US$");
+            decimal _vd = decimal.Parse(Console.ReadLine());
+
+            decimal cv = _vd * vr;
+
+            Console.WriteLine($"US$ {_vd} * R$ {vr} = {_vd * vr}");
+        }
+    }
+}
